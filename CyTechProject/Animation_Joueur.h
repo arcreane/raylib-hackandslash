a36038ecf557d3519185271a4cc29349dbe61ee8
Arthur LACOMBE <lacombeart@cy-tech.fr>
2022-04-26 20:31:52 +0200

    ajout animation joueur immobile

--- CyTechProject/Animation_Joueur.h
+++ CyTechProject/Animation_Joueur.h
@@ -5,6 +5,7 @@ class Animation_Joueur
 private:
 	Texture2D RunLoop0, RunLoop1, RunLoop2, RunLoop3, RunLoop4, RunLoop5;
 	Texture2D Immobile0, Immobile1, Immobile2, Immobile3, Immobile4, Immobile5, Immobile6;
+	Texture2D Jump0, Jump1, Jump2, Jump3, Jump4, Jump5, Jump6;
 	Rectangle frameRecDroite;
 	Rectangle frameRecGauche;
 public:
