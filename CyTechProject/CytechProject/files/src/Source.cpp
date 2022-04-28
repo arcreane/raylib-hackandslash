3e3bace90b23c4a1ac9e58f4b987f09f785f08bf
Maxime Meru <maxime.meru12@outlook.fr>
2022-04-28 23:22:07 +0200

    +3 pour la gestion des platformes c'est mieux et ca permet de ne pas glicth

--- CyTechProject/CytechProject/files/src/Source.cpp
+++ CyTechProject/CytechProject/files/src/Source.cpp
@@ -353,7 +353,7 @@ Joueur UpdatePlayer(Joueur player, Platform platform[NB_PLATFORM], Platform box[
 Joueur CheckCollisionPlatform(Joueur player, Platform platform[NB_PLATFORM], float delta) {
     for (int i = 0; i <= NB_PLATFORM; i++) {
         if (player.getX() > platform[i].getXd() - player.getWidth() && player.getXDroite() <= platform[i].getXDroite() + player.getWidth()
-            && player.getYBas() <= platform[i].getY() && (player.getYBas() + player.getSpeed() * delta +10) >= platform[i].getY()) {
+            && player.getYBas() <= platform[i].getY() && (player.getYBas() + player.getSpeed() * delta +3) >= platform[i].getY()) {
             player.setSpeed(0);
             player.setYBas(platform[i].getY());
             player.setCanJump(true);
