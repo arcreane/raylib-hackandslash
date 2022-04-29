3c2590c1ef462b1e1c3a1dfffdb71636555c8b32
Maxime Meru <maxime.meru12@outlook.fr>
2022-04-29 15:29:19 +0200

    getOrientation() plus de pb

--- CyTechProject/mobPath1.h
+++ CyTechProject/mobPath1.h
@@ -10,7 +10,6 @@ public:
 	MobPath1(Rectangle rec, bool o, float d, float a);
 	void setMob(Rectangle rec, bool o, float d, float a) override; // 0 : sans type, 1 : droite/gauche, 2 : focus perso
 	//bool getOrientation(); // true => , false <=
-	bool getOrientation(); // true => , false <=
 	void pathMob(Joueur player) override;
 	Mob* copy() override;
 };
