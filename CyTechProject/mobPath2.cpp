5966a21f1ff7873e0cf66c7a460d2c5b9ecc47e0
Arthur LACOMBE <lacombeart@cy-tech.fr>
2022-04-29 18:08:17 +0200

    qui a modif l'orientation du ghost?

--- CyTechProject/mobPath2.cpp
+++ CyTechProject/mobPath2.cpp
@@ -26,11 +26,7 @@ void MobPath2::pathMob(Joueur player) {
     float distance = sqrt((playerX - mobX) * (playerX - mobX) + (playerY - mobY) * (playerY - mobY));
     this->setX(this->getX() + 2.5*(playerX - mobX) / distance);
     this->setY(this->getY() + 2.5*(playerY - mobY) / distance);
-    if (this->getX() < 0) {
-        this->setOrientation(true);
-    } else {
-        this->setOrientation(false);
-    }
+
 }
 
 Mob* MobPath2::copy() {
