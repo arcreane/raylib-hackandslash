8da4172ddb2d47121ac9ab27bfa5d6b4092eccc8
Maxime Meru <maxime.meru12@outlook.fr>
2022-04-29 19:01:26 +0200

    error

--- CyTechProject/ArmeDistance.h
+++ CyTechProject/ArmeDistance.h
@@ -10,10 +10,13 @@ private:
 public:
 	void setArme(float radius);
 	void setOn(Vector2 pos);
+	void setOut();
 	void updatePositon();
 
+	Vector2 getPosition();
 	float getX();
 	float getY();
 	float getRadius();
+	bool getActive();
 };
 
